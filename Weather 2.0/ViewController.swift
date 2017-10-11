95c09b762ad27a8ae69e2d93a813d6caf7cf9113
Stefan Ruzic <stefan.ruzic1989@gmail.com>
2017-10-11 11:01:30 +0200

    Initial Commit

---
+++ Weather 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather 2.0
+//
+//  Created by Stefan Ruzic on 11.10.17..
+//  Copyright © 2017. Stefan Ruzic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
