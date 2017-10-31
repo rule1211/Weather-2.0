1b703d93d77bd8344aaa5bb6c404380a5f3745d3
Stefan Ruzic <stefan.ruzic1989@gmail.com>
2017-10-31 16:11:52 +0100

    add TVC and Cell

---
+++ Weather 2.0/Models/Cell.swift
@@ -0,0 +1,9 @@
+//
+//  Cell.swift
+//  Weather 2.0
+//
+//  Created by Stefan Ruzic on 31.10.17..
+//  Copyright © 2017. Stefan Ruzic. All rights reserved.
+//
+
+import Foundation
---
+++ Weather 2.0/Models/TVCell.swift
@@ -0,0 +1,24 @@
+//
+//  TVCell.swift
+//  Weather 2.0
+//
+//  Created by Stefan Ruzic on 31.10.17..
+//  Copyright © 2017. Stefan Ruzic. All rights reserved.
+//
+
+import UIKit
+
+class TVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
